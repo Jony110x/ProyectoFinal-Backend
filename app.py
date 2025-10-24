c5122daea421dd79b65c1953620dbb18f7bf89a2
unknown <hola.com>
2025-10-23 23:19:38 -0300

    canbie las normas de CORS

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ api_escu.include_router(asignar)
 api_escu.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"]
 )
